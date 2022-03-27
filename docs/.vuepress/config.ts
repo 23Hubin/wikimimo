09c90892fc444f84958d0ddf6b076bca1721cc71
imgradeone - 一年级么么哒 <imgradeone@outlook.com>
2022-03-27 22:37:16 +0800

    init

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,12 @@
+import { defineHopeConfig } from "vuepress-theme-hope";
+
+export default defineHopeConfig({
+    lang: 'zh-CN',
+    title: 'WikiMimo',
+    description: 'Just playing around',
+
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+      // 此处放置主题配置
+    },
+  });
